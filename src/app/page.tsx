e0da1de3c5ab9cdf4a5b6701f1d7699c5072ea40
Ali <ahmmsel2@gmail.com>
2026-01-15 18:52:07 +0200

    Initial setup for prisma and database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-export default function Page() {
+export default async function Page() {
+  const users = await prisma.user.findMany();
+
+  console.log("Users:", users);
   return (
     <div className="flex items-center justify-center min-h-screen">
       <Button>Click Me</Button>
