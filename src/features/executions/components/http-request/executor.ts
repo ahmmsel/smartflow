f96350f1fc3f25dca3a620cc73dc3e1a2cae53ed
Ali <ahmmsel2@gmail.com>
2026-01-28 21:01:07 +0200

    feat: add node templating functionality

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,13 +1,19 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import axios from "axios";
+import Handlebars from "handlebars";
 
 type HttpRequestData = {
-  endpoint?: string;
-  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-  body?: unknown;
   variableName: string;
+  endpoint: string;
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  body?: unknown;
 };
+
+Handlebars.registerHelper("json", function (context) {
+  const jsonString = JSON.stringify(context, null, 2);
+  return new Handlebars.SafeString(jsonString);
+});
 
 export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   data,
@@ -19,6 +25,12 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
     throw new NonRetriableError('HTTP Request node requires an "endpoint"');
   }
 
+  if (!["GET", "POST", "PUT", "PATCH", "DELETE"].includes(data.method)) {
+    throw new NonRetriableError(
+      `HTTP Request node has invalid "method": ${data.method}`,
+    );
+  }
+
   if (!data.variableName) {
     throw new NonRetriableError(
       'HTTP Request node requires a "variableName" to store the response',
@@ -26,28 +38,42 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   }
 
   const result = await step.run("http-request", async () => {
-    const endpoint = data.endpoint!;
-    const method = data.method || "GET";
+    const endpoint = Handlebars.compile(data.endpoint)(context);
+    const method = data.method;
 
     const options = {
       method: method,
       data: undefined as unknown,
       headers: undefined as Record<string, string> | undefined,
+      body: undefined as unknown,
     };
 
-    if (["POST", "PUT", "PATCH"].includes(method)) {
-      if (data.body) {
-        options.data = data.body;
-        options.headers = {
-          "Content-Type": "application/json",
-        };
-      }
+    if (["POST", "PUT", "PATCH"].includes(method) && data.body) {
+      const resolved = Handlebars.compile(
+        typeof data.body === "string" ? data.body : JSON.stringify(data.body),
+      )(context);
+
+      const parsedBody = JSON.parse(resolved);
+
+      options.data = parsedBody;
+      options.headers = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      };
     }
 
     const response = await axios(endpoint, {
       method,
       data: options.data,
+      headers: options.headers,
       responseType: "json",
+    });
+
+    console.log("REQUEST", {
+      endpoint,
+      method,
+      headers: options.headers,
+      data: options.data,
     });
 
     const contentType = response.headers["content-type"] || "";
